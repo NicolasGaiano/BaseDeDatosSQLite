9373c00027c84b1cde90343b0dc73ffd701907aa
Nicolas Gaiano <nicolasgaiano671@gmail.com>
2022-10-02 19:02:12 -0300

    listo para agregar funcionalidad a la interfaz grafica

---
+++ BaseDeDatosSQLite/BaseDeDatosSQLite/ClasesDB/Cliente.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SQLite;
+
+namespace BaseDeDatosSQLite.ClasesDB
+{
+    public class Cliente
+    {
+        //Representacion de la Tabla creada en SQLite 
+        
+        public int NCliente { get; set; }
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string Email { get; set; }
+
+    }
+    public class ClienteLogic
+    {
+        //Cadena de conexion 
+        
+        private static string cadenaDeConexion = ConfigurationManager.ConnectionStrings["cadenaDeConexion"].ConnectionString;
+
+        private static ClienteLogic _instancia = null;
+
+        public ClienteLogic() { 
+        
+        }
+
+        //patron de diseño singleton.
+        //asegurara de que la clase ClienteLogic tenga una única instancia
+        public static ClienteLogic Instancia
+        {
+            get {
+                if( _instancia == null) { 
+                _instancia = new ClienteLogic();
+                }
+            return _instancia;
+            }
+            
+        }
+
+        public bool Guardar(Cliente obj) 
+        {
+            bool respuesta = true;
+
+            using (SQLiteConnection conexion = new SQLiteConnection(cadenaDeConexion))
+            {
+                conexion.Open();
+                string consulta = "insert into Cliente(Nombre,Apellido,Telefono,Email) values (@nombre,@apellido,@telefono,@email)";
+
+                SQLiteCommand cmd = new SQLiteCommand(consulta, conexion);
+                cmd.Parameters.Add(new SQLiteParameter("@nombre", obj.Nombre));
+                cmd.Parameters.Add(new SQLiteParameter("@apellido", obj.Apellido));
+                cmd.Parameters.Add(new SQLiteParameter("@telefono", obj.Telefono));
+                cmd.Parameters.Add(new SQLiteParameter("@email", obj.Email));
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                //si no hubo ninguna inserción, respuesta = false
+                if (cmd.ExecuteNonQuery() < 1)
+                {
+                    respuesta=false;
+                }
+            }
+
+                return respuesta;
+        }
+
+        public List<Cliente> ListarClientes()
+        {
+            List<Cliente> lista = new List<Cliente>();
+
+            using (SQLiteConnection conexion = new SQLiteConnection(cadenaDeConexion))
+            {
+                conexion.Open();
+                string consulta = "select * from Cliente";
+
+                SQLiteCommand cmd = new SQLiteCommand(consulta, conexion);
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                using (SQLiteDataReader dataR = cmd.ExecuteReader())
+                {
+                    while (dataR.Read())
+                    {
+                        lista.Add(new Cliente()
+                        {
+                            NCliente = int.Parse(dataR["N° Cliente"].ToString()),
+                            Nombre = dataR["Nombre"].ToString(),
+                            Apellido = dataR["Apellido"].ToString(),
+                            Telefono = dataR["Telefono"].ToString(),
+                            Email = dataR["Email"].ToString(),
+                        }) ;
+                    }
+
+
+                }
+            }
+
+            return lista;
+        }
+
+    }
+
+}
--- BaseDeDatosSQLite/BaseDeDatosSQLite/Form1.Designer.cs
+++ BaseDeDatosSQLite/BaseDeDatosSQLite/Form1.Designer.cs
@@ -28,12 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNCliente = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.dgvClientes = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "N° Cliente";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Apellido";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Telefono";
+            // 
+            // txtNCliente
+            // 
+            this.txtNCliente.Location = new System.Drawing.Point(68, 16);
+            this.txtNCliente.Name = "txtNCliente";
+            this.txtNCliente.Size = new System.Drawing.Size(420, 23);
+            this.txtNCliente.TabIndex = 4;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(68, 45);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(420, 23);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(68, 74);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(420, 23);
+            this.txtApellido.TabIndex = 6;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(68, 103);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(420, 23);
+            this.txtTelefono.TabIndex = 7;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(68, 132);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(420, 23);
+            this.txtEmail.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Email";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(528, 24);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(129, 34);
+            this.btnGuardar.TabIndex = 10;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(528, 72);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(129, 34);
+            this.btnEditar.TabIndex = 11;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(528, 121);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(129, 34);
+            this.btnEliminar.TabIndex = 12;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // dgvClientes
+            // 
+            this.dgvClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClientes.Location = new System.Drawing.Point(12, 176);
+            this.dgvClientes.Name = "dgvClientes";
+            this.dgvClientes.RowTemplate.Height = 25;
+            this.dgvClientes.Size = new System.Drawing.Size(645, 249);
+            this.dgvClientes.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(669, 437);
+            this.Controls.Add(this.dgvClientes);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtNCliente);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox txtNCliente;
+        private TextBox txtNombre;
+        private TextBox txtApellido;
+        private TextBox txtTelefono;
+        private TextBox txtEmail;
+        private Label label5;
+        private Button btnGuardar;
+        private Button btnEditar;
+        private Button btnEliminar;
+        private DataGridView dgvClientes;
     }
 }
